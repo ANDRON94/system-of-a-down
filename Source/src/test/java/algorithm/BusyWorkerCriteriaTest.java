864b4986a8df7492cdcbd917c85a6e04c697ad28
ANDRON94 <makedonsky94@gmail.com>
2015-04-28 16:03:30 +0300

    - Add test for busy worker criteria;
    - Add test for deadline work criteria;
    
    - Add test for duration operation criteria;

---
+++ Source/src/test/java/algorithm/BusyWorkerCriteriaTest.java
@@ -0,0 +1,61 @@
+package algorithm;
+
+import com.model.Detail;
+import com.model.DetailType;
+import com.model.Order;
+import com.model.Worker;
+import com.repository.OrderRepository;
+import com.repository.WorkerRepository;
+import com.service.algorithms.schedule.BusyWorkerCriteria;
+import com.service.algorithms.schedule.DurationOperationCriteria;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * Created by andron94 on 27.04.15.
+ */
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"/database.xml"})
+public class BusyWorkerCriteriaTest {
+    @Autowired
+    WorkerRepository workerRepository;
+    @Autowired
+    OrderRepository orderRepository;
+
+    @Test
+    public void testBusyWorkerCriteria(){
+        Date date = null;
+        try {
+            date= new SimpleDateFormat("yyyy-mm-dd").parse("2012-05-9 14:00:00");
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        List<Worker> workers = workerRepository.findAll();
+        BusyWorkerCriteria busyWorkerCriteria = new BusyWorkerCriteria(workers,null);
+        Order order = orderRepository.findByStartAfterAndOrderStatusName("NEW_ORDER", date).get(0);
+        DurationOperationCriteria durationOperationCriteria = new DurationOperationCriteria();
+        List<Detail> details = durationOperationCriteria.find(order);
+        for (Detail detail : details) {
+            Worker worker = busyWorkerCriteria.find(detail);
+            System.out.println("Detail Id: " + detail.getId());
+            System.out.println("Detail Type: "+ detail.getDetailType().getName() );
+            System.out.println("Detail Time: "+ detail.getDetailType().getProduceTime() );
+
+            System.out.println("Worker Name: " + worker.getName() );
+            System.out.println("Wokrer spec: " );
+            for( DetailType spec : worker.getSpecializations() ){
+                System.out.print(spec.getName() + " ");
+            }
+            System.out.println();
+        }
+    }
+}
